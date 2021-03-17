02e4f71b32b078bf385354a3a0bd709b7d18fc9d
Leo Woolcock <leo.woolcock@gmail.com>
2021-03-17 15:14:59 +0000

    tweaks

---
+++ README.md
@@ -0,0 +1,31 @@
+# jitsi-puppeteer-basic
+A basic version of my Jitsi Puppeteer implementation
+
+*THIS CODE IS UNTESTED AND HAS BEEN PRODUCED FOR DEMO PURPOSES ONLY. IT PROBABLY WILL NOT WORK STRAIGHT OUT OF THE BOX*
+
+
+
+You will need to install certain things on your server for this to work at all:
+
+  sudo apt install npm chromium-browser nginx ffmpeg -y
+
+  sudo apt-get install gconf-service libasound2 libatk1.0-0 libatk-bridge2.0-0 libc6 libcairo2 libcups2 libdbus-1-3 libexpat1 libfontconfig1 libgcc1 libgconf-2-4 libgdk-pixbuf2.0-0 libglib2.0-0 libgtk-3-0 libnspr4 libpango-1.0-0 libpangocairo-1.0-0 libstdc++6 libx11-6 libx11-xcb1 libxcb1 libxcomposite1 libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss1 libxtst6 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
+
+
+Then, within the directory of the package:
+
+  sudo npm install
+
+
+To convert videos in MP4 format to Y4M and WAV:
+  
+  ffmpeg -i "test.mp4" -pix_fmt yuv420p -vf scale=320:180 -r 15 -map "0:v" "test.y4m"
+  ffmpeg -i "test.mp4" -af asetrate=48000,aresample=48000 "test.wav"
+
+===========
+
+Copyright 2021 L-Wo 
+
+Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
+
+THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
--- app.js
+++ app.js
@@ -1,82 +1,91 @@
+//THIS CODE IS UNTESTED AND HAS BEEN PRODUCED FOR ILLUSTRATIVE PURPOSES ONLY
+
 const puppeteer = require('puppeteer');
-const jitsiBaseUrl = "YOUR_JITSI_FQDN_HERE"
-const conferenceRoomName = "YOUR_CONERENCE_ROOM_NAME"
-const displayName = "YOUR_DISPLAY_NAME"
+const jitsiBaseUrl = "https://meet.jit.si" //e.g. https
+const conferenceRoomName = "mytestroom987" //e.g. room1
+const displayName = "Bot Demo"
 const ttl = 30000; //How long before we kill off the video - otherwise it will loop
+const videoFile = "/home/videos/test.y4m" //See readme for details on Y4M conversions
+const audioFile = "/home/videos/test.wav" //Must be a .wav. Not sure of other constraints
 
-        const meetArgs = [
-                // Disable receiving of video
-                'config.channelLastN=0',
-                'config.constraints.video.height.min=120',
-                // Turn off some bits of audio processing and unmute
-                'config.disableAP=true','config.disableHPF=true&config.diableNS=true&config.disableAGC=true','config.startWithAudioMuted=false',
-                // Don't use simulcast to save resources on the sender (our) side
-                'config.disableSimulcast=true',
-                // No need to process audio levels
-                'config.disableAudioLevels=true',
-                // Disable P2P mode due to a bug in Jitsi Meet
-                'config.p2p.enabled=false',
-                // Tell the system we are a bot so it shouldn't clear our localstorage before proceeding
-                'config.iAmBot=true',
-                //Skip prejoin page
-                'config.prejoinPageEnabled=false',
-                //Disable playing of other videos - Remember to turn this off if you use this to do screenshots!
-                'config.testing.noAutoPlayVideo',
-                //Set local video resolution low to save cpu
-                'config.resolution=120',
-                //only show filmstrip
-                'interfaceConfig.filmStripOnly=true',
+async function main(){
 
-        ];
+const meetArgs = [
+    // Disable receiving of video
+    'config.channelLastN=0',
+    'config.constraints.video.height.min=120',
+    // Turn off some bits of audio processing and unmute
+    'config.disableAP=true', 'config.disableHPF=true&config.diableNS=true&config.disableAGC=true', 'config.startWithAudioMuted=false',
+    // Don't use simulcast to save resources on the sender (our) side
+    'config.disableSimulcast=true',
+    // No need to process audio levels
+    'config.disableAudioLevels=true',
+    // Disable P2P mode
+    'config.p2p.enabled=false',
+    //Skip prejoin page
+    'config.prejoinPageEnabled=false',
+    //Disable playing of other videos - Remember to turn this off if you use this to do screenshots!
+    'config.testing.noAutoPlayVideo',
+    //Set local video resolution low to save cpu
+    'config.resolution=120',
+    //only show filmstrip
+    'interfaceConfig.filmStripOnly=true',
 
-        const chromeArgs = [
-                // Disable sandboxing, gives an error on Linux
-                '--no-sandbox',
-                '--disable-setuid-sandbox',
-                // Automatically give permission to use media devices
-                '--use-fake-ui-for-media-stream',
-                // Performance helping flags
-                '--disable-dev-shm-usage',
-                '--disable-accelerated-2d-canvas',
-                '--no-first-run',
-                '--no-zygote',
-                '--single-process',
-                '--disable-gpu',
-                '--window-size=400,300',
-                '--disable-web-security',
-                '--use-fake-device-for-media-stream',
-                '--use-file-for-fake-video-capture='+videoFolder+file+'.y4m',
-                // Use a file as the audio device
-                '--use-file-for-fake-audio-capture='+videoFolder+file+'.wav',
-        ];
-      
-        //Init browser      
-        const browser = await puppeteer.launch({ args: chromeArgs, headless: true });
+];
 
-		//Open new page
-        const page = await browser.newPage();
+const chromeArgs = [
+    // Disable sandboxing, gives an error on Linux
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    // Automatically give permission to use media devices
+    '--use-fake-ui-for-media-stream',
+    // Performance helping flags
+    '--disable-dev-shm-usage',
+    '--disable-accelerated-2d-canvas',
+    '--no-first-run',
+    '--no-zygote',
+    '--single-process',
+    '--disable-gpu',
+    '--window-size=400,300',
+    '--disable-web-security',
+    '--use-fake-device-for-media-stream',
+    '--use-file-for-fake-video-capture=' + videoFile,
+    // Use a file as the audio device
+    '--use-file-for-fake-audio-capture=' + audioFile,
+];
 
-        //Navigate to the conference
-		console.log('Joining Conference...');
-        const url = `${jitsiBaseUrl}/${conferenceRoomName}#${meetArgs.join('&')}&userInfo.displayName=%22${displayName}%22`;
-        page.goto(url)
+//Init browser      
+const browser = await puppeteer.launch({ args: chromeArgs, headless: true });
 
-		//Abort any uneccessary requests to save resources
-        page.setRequestInterception(true);
-        page.on('request', request => {
-            const resource = request.resourceType().toUpperCase();
-            if (resource === 'IMAGE' || resource === 'MEDIA'){
-                request.abort();
-            } else {
-                request.continue();
-            }
-        });
-		console.log("Connected, now waiting for ttl in ms: ", ttl)
-		setTimeout(async () => {
-			console.log('Hanging up...')
-            await page.evaluate('APP.conference.hangup();')
-            await page.close()
-            await browser.close();
-		}, ttl)
-		return true; //Returns true once it's joined and set the timer for expiry
-	}
+//Open new page
+const page = await browser.newPage();
+
+//Navigate to the conference
+console.log('Joining Conference...');
+const url = `${jitsiBaseUrl}/${conferenceRoomName}#${meetArgs.join('&')}&userInfo.displayName=%22${displayName}%22`;
+page.goto(url)
+
+
+//Abort any uneccessary requests to save resources
+page.setRequestInterception(true);
+page.on('request', request => {
+    const resource = request.resourceType().toUpperCase();
+    if (resource === 'IMAGE' || resource === 'MEDIA') {
+        request.abort();
+    } else {
+        request.continue();
+    }
+});
+
+//Set the bot to automatically kill itself after the TTL
+console.log("Connected, now waiting for ttl in ms: ", ttl)
+setTimeout(async () => {
+    console.log('Hanging up...')
+    await page.evaluate('APP.conference.hangup();')
+    await page.close()
+    await browser.close();
+}, ttl)
+
+}
+
+main();
